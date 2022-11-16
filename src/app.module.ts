9b772017045a3e96d0786a5df585bdc7344aed39
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-11-16 09:53:35 +0900

    add: graphql root 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {GraphQLModule} from "@nestjs/graphql";
 
+/**
+ * forRoot?
+ */
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GraphQLModule.forRoot()],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/**
+ * appModule은 main.ts로 import 되는 유일한 모듈
+ * -> appModule에 graphQL 을 추가
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
