65a8bfea0690ee08ba9a9d095a9f98169e2f79c1
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-11-16 11:13:41 +0900

    add: validate arguments using class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 
 /**
  * appModule은 main.ts로 import 되는 유일한 모듈
@@ -7,6 +8,10 @@ import { AppModule } from './app.module';
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+      new ValidationPipe()
+  )
   await app.listen(3000);
 }
+
 bootstrap();
