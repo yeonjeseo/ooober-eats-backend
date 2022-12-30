70936aa57b5e083663187014e65fe3df6b272742
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-12-30 13:56:11 +0900

    add: import .env using ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,27 @@ import {GraphQLModule} from "@nestjs/graphql";
 import { ApolloDriver } from '@nestjs/apollo';
 import {RestaurantsModule} from "./restaurants/restaurants.module";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
 /**
  * forRoot?
  */
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: process.env.NODE_ENV === 'dev' ? ".env.dev" : '.env.test',
+        ignoreEnvFile: process.env.Node_ENV === 'prod'
+      }),GraphQLModule.forRoot({
     driver: ApolloDriver,
     autoSchemaFile: true,
   }),
     RestaurantsModule,
   TypeOrmModule.forRoot({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "jeffrey",
-    password: "admin",
-    database: "ooober-eats",
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port:  +process.env.DB_PORT,
+    password: process.env.DB_USERNAME,
+    database: process.env.DB_DATABASE,
     entities: [],
     synchronize: true,  // DB를 현재 모듈 상태로 동기화
     logging: true,
