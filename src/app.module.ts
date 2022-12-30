5a91cf19bd79d1769f2be1c404139021e4c2dccf
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-12-30 14:06:47 +0900

    add: validate environment variables with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,25 @@ import { ApolloDriver } from '@nestjs/apollo';
 import {RestaurantsModule} from "./restaurants/restaurants.module";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ConfigModule} from "@nestjs/config";
+import * as Joi from 'joi'
 /**
  * forRoot?
  */
+
 @Module({
   imports: [
       ConfigModule.forRoot({
         isGlobal: true,
         envFilePath: process.env.NODE_ENV === 'dev' ? ".env.dev" : '.env.test',
-        ignoreEnvFile: process.env.Node_ENV === 'prod'
+        ignoreEnvFile: process.env.Node_ENV === 'prod',
+        validationSchema: Joi.object({
+          NODE_ENV: Joi.string().required().valid('dev', 'test', 'prod'),
+          DB_HOST: Joi.string().required(),
+          DB_PORT: Joi.string().required(),
+          DB_USERNAME: Joi.string().required(),
+          DB_PASSWORD: Joi.string().required(),
+          DB_DATABASE: Joi.string().required(),
+        })
       }),GraphQLModule.forRoot({
     driver: ApolloDriver,
     autoSchemaFile: true,
@@ -22,7 +32,8 @@ import {ConfigModule} from "@nestjs/config";
     type: 'postgres',
     host: process.env.DB_HOST,
     port:  +process.env.DB_PORT,
-    password: process.env.DB_USERNAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: [],
     synchronize: true,  // DB를 현재 모듈 상태로 동기화
