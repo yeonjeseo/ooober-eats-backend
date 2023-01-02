0cbe16974adc423fd689dd6b9d545a1cae6fa17a
Yeon Jeffrey <yeonjeseo@hotmail.com>
2023-01-02 15:16:24 +0900

    add: inject config service to user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from './entities/users.entity'
 import {UsersResolver} from "./users.resolver";
 import {UsersService} from "./users.service";
+import { ConfigService} from "@nestjs/config";
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    providers: [UsersResolver, UsersService]
+    providers: [UsersResolver, UsersService, ConfigService]
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,15 @@ import {User} from "./entities/users.entity";
 import {Injectable, InternalServerErrorException} from "@nestjs/common";
 import {CreateAccountInput, CreateAccountOutput} from "./dtos/create-account.dto";
 import {LoginInput, LoginOutput} from "./dtos/login.dto";
-import * as bcrypt from 'bcrypt'
-
+// import * as bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
+import {ConfigModule, ConfigService} from "@nestjs/config";
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private readonly users: Repository<User>) {}
+    constructor(@InjectRepository(User) private readonly users: Repository<User>,
+                private readonly config: ConfigService) {
+        console.log(this.config.get('JWT_SECRET'))
+    }
 
     async createAccount({email, password, role}: CreateAccountInput): Promise<CreateAccountOutput> {
         try {
@@ -51,9 +55,14 @@ export class UsersService {
                 error: 'Wrong password!'
             }
 
+            /**
+             * process.env를 사용하는 것이 아니라 Nest 적인 방법을 사용할 것
+             * users.module
+             *
+             */
             return {
                 ok: true,
-                token: 'asdkjaksjdlakj'
+                token: jwt.sign({id: user.id}, this.config.get('JWT_SECRET'))
             }
         }catch (e) {
             return {
