b85a73f1435580221cfac062190d93059d56284a
Yeon Jeffrey <yeonjeseo@hotmail.com>
2023-01-02 14:00:09 +0900

    feat: implemented hash by @BeforeInsert decorator

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,7 +1,8 @@
-import {Column, Entity} from "typeorm";
+import {BeforeInsert, Column, Entity} from "typeorm";
 import {CoreEntity} from "../../common/entities/core.entity";
 import {Field, InputType, ObjectType, registerEnumType} from "@nestjs/graphql";
-
+import * as bcrypt from 'bcrypt'
+import {InternalServerErrorException} from "@nestjs/common";
 enum UserRole {
     Owner,
     Client,
@@ -25,4 +26,18 @@ export class User extends CoreEntity {
     @Field(type => UserRole)
     @Column({type: 'enum', enum: UserRole})
     role: UserRole;
+
+    /**
+     * Entity Listener
+     * "특정 Entity event 를 Listen 하는 사용자 로직이 있는 method 를 가질 수 있다.
+     */
+    @BeforeInsert()
+    async hashPassword () : Promise<void> {
+        try {
+            this.password = await bcrypt.hash(this.password, 10)
+        } catch (e) {
+            console.log(e);
+            throw new InternalServerErrorException();
+        }
+    }
 }
