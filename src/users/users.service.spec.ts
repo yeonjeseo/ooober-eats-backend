32738aa1ea49ec1d4999305f8dbedef7f6041a5b
Yeon Jeffrey <yeonjeseo@hotmail.com>
2023-01-04 10:37:03 +0900

    add: mock user repository

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -5,6 +5,7 @@ import { User } from './entities/users.entity';
 import { Verification } from './entities/verification.entity';
 import { JwtService } from '../jwt/jwt.service';
 import { MailService } from '../mail/mail.service';
+import { Repository } from 'typeorm';
 
 const mockRepository = {
   findOne: jest.fn(),
@@ -21,8 +22,13 @@ const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<
+  Record<keyof Repository<User>, jest.Mock>
+>;
+
 describe('UserService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository<User>;
 
   /**
    * 모든 테스트 케이스에 대해 테스트 모듈 생성
@@ -51,13 +57,24 @@ describe('UserService', () => {
       ],
     }).compile();
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('createAccount');
+  /**
+   * createAccount 함수를 분리해서 테스트
+   * 함수의 모든 부분을 테스트
+   * createAccount는 어떻게 테스트 할거야?
+   * 1. 만들고자 하는 유저 정보 생성 -> ok: true | ok: false -> 함수의 출력물, 결과에 대한 테스트이고 E2E 테스트
+   * 2. 유닛테스트에서 하고자 하는 건 코드의 각 줄에 문제가 없는지 확인
+   */
+  // it.todo('createAccount');
+  describe('createAccount', () => {
+    it('should fail if user exists', () => {});
+  });
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
